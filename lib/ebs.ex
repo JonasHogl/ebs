03989c4a345a43eeafc100e651e130b581752090
JonasHogl <jonas_hoglund_@live.com>
2024-10-07 21:27:03 +0200

    mekimek

--- lib/ebs.ex
+++ lib/ebs.ex
@@ -3,16 +3,26 @@ defmodule Ebs do
   Documentation for `Ebs`.
   """
 
-  @doc """
-  Hello world.
+  def kör do
+    nyckel = hämta_nyckel()
 
-  ## Examples
+    ohanterade_produkter = hämta_produkter(nyckel)
 
-      iex> Ebs.hello()
-      :world
+    produkter = mappa_produkter(ohanterade_produkter)
 
-  """
-  def hello do
-    :world
+    persistera_produkter(produkter)
+  end
+
+  def hämta_produkter(nyckel) do
+  end
+
+  def hämta_nyckel do
+    Req.get!("https://www.systembolaget.se").body
+  end
+
+  def mappa_produkter(råa_produkter) do
+  end
+
+  def persistera_produkter(produkter) do
   end
 end
